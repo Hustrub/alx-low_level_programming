c3af59e1427bc6c42324d0bbede9d66017a32041
hustrub <hustrubyte@gmail.com>
2023-06-15 21:44:52 -0700

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,3 @@
-#include <stuio.h>
+#include <stdio.h>
 /**
- *main - Entry point
-*Return: Always 0 (success)
-*/
-int main(void)
-{
- puts("\"Programming is like building a multilingual puzzle");
- return(0);
-}
+* main - Entry point
