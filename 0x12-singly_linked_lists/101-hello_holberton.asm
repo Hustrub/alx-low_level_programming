d56be1917aad7eafd23889d21caa9e80f45af488
hustrub <hustrubyte@gmail.com>
2023-07-27 13:34:29 -0700

    all task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,33 @@
+global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
+
+
+
+=====================================. If 11-hello failed to check
+You can try the code below
+
+
+
+global main
+extern printf
+
+	section .text
+main:
+	push rbp
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0
+	call printf
+	pop rbp
+	mov rax, 0
+	ret
+message: db "Hello, Holberton", 0
+format:	db "%s", 10, 0
+
