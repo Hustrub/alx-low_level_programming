255fcbda6be3b87856b0926fe59fd29eede51967
hustrub <hustrubyte@gmail.com>
2023-07-27 13:59:51 -0700

    last task written

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,3 @@
-global   main
-	extern    printf
-main:
-	mov   edi, format
-	xor   eax, eax
-	call  printf
-	mov   eax, 0
-	ret
-format: db `Hello, Holberton\n`,0
-
-
-
-=====================================. 
-If 11-hello failed to check
-You can try the code below
-
-
-
 global main
 extern printf
 
