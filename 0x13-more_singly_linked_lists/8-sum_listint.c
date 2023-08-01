dac1b4bb3af31bc04facb3d120ad36d181dfdd93
hustrub <hustrubyte@gmail.com>
2023-08-01 10:40:19 -0700

    double

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - cal d sum of all data in a listint_t list of C program
+ * @head: first node in the linked list
+ * author &hustrub
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+listint_t *current = head;
+
+while (current != NULL)
+{
+sum += current->data;
+current = current->next;
+}
+
+return (sum);
+}
+
