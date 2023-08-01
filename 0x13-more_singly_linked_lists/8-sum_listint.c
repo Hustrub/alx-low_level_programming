28dfe5a2a2135185a101172dbc540cf51cad0a8a
hustrub <hustrubyte@gmail.com>
2023-08-01 11:00:53 -0700

    887yy

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,7 +3,7 @@
 /**
  * sum_listint - cal d sum of all data in a listint_t list of C program
  * @head: first node in the linked list
- * author &hustrub
+ * author hustrub
  * Return: resulting sum
  */
 int sum_listint(listint_t *head)
@@ -11,7 +11,7 @@ int sum_listint(listint_t *head)
 int sum = 0;
 listint_t *current = head;
 
-while (current != NULL)
+while (current)
 {
 sum += current->data;
 current = current->next;
