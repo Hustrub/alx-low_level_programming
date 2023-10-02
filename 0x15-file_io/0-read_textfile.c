5f2abb2bb12e62b5d7592715d447b795f44433f7
hustrub <hustrubyte@gmail.com>
2023-10-02 11:34:09 -0700

    check

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
 	t = read(fd, buff, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	w = write(STDOUT_FILENO, buff, t);
 
 	free(buff);
 	close(fd);
