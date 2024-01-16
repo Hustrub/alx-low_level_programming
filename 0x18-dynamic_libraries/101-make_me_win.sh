c58a0c3baa53994fb8307e84fcd70a296183e9ed
hustrub <hustrubyte@gmail.com>
2024-01-15 18:51:06 -0800

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/hustrub/alx-low_level_programming/raw/master/0x18-dynamic_libraries/liball.so
+wget -p /tmp https://github.com/Hustrub/alx-low_level_programming/blob/master/0x18-dynamic_libraries/liball.so
 export LD_PRELOAD=/tmp/liball.so
